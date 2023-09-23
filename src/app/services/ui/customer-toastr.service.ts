ccf066051c29108d2c34c13331992fd810fb8404
guven.uysal <gvnuysal@gmail.com>
2023-09-23 13:38:28 +0300

    toastr ek işlemler

---
+++ src/app/services/ui/customer-toastr.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CustomerToastrService {
+
+  constructor(private toastrService: ToastrService) { }
+  message(message: string, title: string,toastrOptions:Partial<ToastrOptions>) {
+    this.toastrService[toastrOptions.messageType](message, title, {
+      positionClass:toastrOptions.position
+    })
+  }
+}
+export class ToastrOptions{
+  messageType: ToasterMessageType
+  position:ToasterPositionType
+}
+export enum ToasterMessageType{
+  Success = 'success',
+  Info = 'info',
+  Warning = 'warning',
+  Error='error'
+}
+export enum ToasterPositionType{
+  TopRight='toast-top-right',
+  BottomRight='toast-bottom-right',
+  BottomLeft='toast-bottom-left',
+  TopLeft='toast-top-left',
+  TopFullWidth='toast-top-full-width',
+  BottomFullWidth='toast-bottom-full-width',
+  TopCenter='toast-top-center',
+  BottomCenter='toast-bottom-center'
+}
