5b785d9288dfa7c6ac373d8479394c7e0cd5376b
guven.uysal <gvnuysal@gmail.com>
2025-01-11 20:16:08 +0300

    fluent

--- src/app/services/common/models/products/product.service.ts
+++ src/app/services/common/models/products/product.service.ts
@@ -1,9 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClientService } from '../../http-client.service';
 import { VM_Create_Product } from './product.model';
-import { HttpErrorResponse } from '@angular/common/http';
-import { ValueConverter } from '@angular/compiler/src/render3/view/template';
-
+import { HttpErrorResponse } from '@angular/common/http'; 
 @Injectable({
   providedIn: 'root'
 })
@@ -21,12 +19,7 @@ export class ProductService {
       const _error: Array<{ key: string, value: Array<string> }> = errorResponse.error
       let message = "";
       console.log(_error["errors"])
-      message=_error["errors"]
-      // _error["errors"].forEach((v, index) => {
-      //   v.value.forEach((_v, _index) => {
-      //     message+=`${_v}<br>`
-      //   })
-      // })
+      message=_error["errors"] 
       errorCallBack(message);
     })
   }
